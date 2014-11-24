7ab63f1b7d2d114653ed2cec48d6f43a2621c590
drb8w <drb8w@directbox.com>
2014-11-24 18:15:56 +0100

    recent changes - online LMS

--- Aufgabe01/MyLinearRegression.m
+++ Aufgabe01/MyLinearRegression.m
@@ -13,8 +13,8 @@ function MyLinearRegression()
     [x_t,t]=generateTrainingsSet(x,y,G,my,Sigma);
 
     y_t = t';
-    lamda = 0.0001;
-
+    lambda = 0.0001;
+    
     %%A_3 = createA(x_t,3);
     %%w_star_3 = compute_w_star(A_3,y_t,lamda);
     %%y_star_3 = createPolynomValues(x,w_star_3);
@@ -27,15 +27,22 @@ function MyLinearRegression()
     %
     dimension_start = 3;
     dimension_end = 9;
-    W_star = trans_x_comp_w_star(x_t,y_t,lamda,dimension_start, dimension_end);
+    W_star = trans_x_comp_w_star(x_t,y_t,lambda,dimension_start, dimension_end);
     Y = createPolynomValuesW(x,W_star);
 
-    plotDataY(x,Y);
+    hold on %%DAN
+    plotDataY(x,Y,'-', 'r'); %%DAN
+    plotDataY(x_t, t,'o', 'g'); %%DAN
+    %plotDataY(x_t, t,'-', 'g'); 
+    hold off %%DAN
 
-    threshold = 0.01;
-    my = 0.05;
+    % test of online LMS
+    E_threshold = 0.01;
+    lambda = 0.05;
     A_3 = createA(x_t,3);
-    w_online_3 = onlineLMS(A_3, y_t ,my, threshold);
+    w_online_3 = onlineLMS(A_3, y_t ,lambda, E_threshold);
+    Y_online = createPolynomValuesW(x,w_online_3);
+    
 
 end
 
@@ -46,12 +53,20 @@ end
 
 function [x_t,t] = generateTrainingsSet(x,y,G,my,Sigma)
     % Y=grand(m,n,'nor',Av,Sd) generates random variates from the normal distribution with mean Av (real) and standard deviation Sd (real >= 0)
+
+    %%DAN (-> nur falls 6 random punkte)
+    %%x_t = x(randperm(length(x)));
+    %%x_t = sort(x_t(1:6));
     x_t = x(1:6:end);
+    
     [m,n] = size(x_t);
     % Scilab
     %noise = grand(m,n,'nor',my, Sigma);
     % Matlab
-    noise = normrnd(my, Sigma,m,n);
+    %noise = normrnd(my, Sigma,m,n);
+    
+    %% DAN matlab < 2014:
+    noise = randn(m,n)*Sigma + my;
     y_t = 2*x_t.^2-G*x_t+1;
     t = y_t + noise;    
 end
@@ -63,19 +78,23 @@ function plotData(x,y,x_t,t,y_star, y_star_II)
     %set(gca(),"auto_clear","on");    
 end
 
-function plotDataY(x,Y)
+function plotDataY(x,Y, mode, color)
     plotColors = ['r','g','b','c','m','y'];
     [m,n] = size(Y);
     [i,j] = size(plotColors);
-    clf;
+    %clf;
     %set(gca(),"auto_clear","off");
-    hold on;
+    %hold on;%DAN
     for index=1:m
-        plotArg = strcat([plotColors(mod(index-1,j)+1),'-']);
+        if(nargin<4)
+            plotArg = strcat([plotColors(mod(index-1,j)+1),mode]);
+        else
+            plotArg = strcat([color,mode]);
+        end
         %plot(x,Y(index,:),'ro-');
         plot(x,Y(index,:),plotArg);
     end
-    hold off;
+    %hold off;%DAN
     %set(gca(),"auto_clear","on");    
 end
 
@@ -143,25 +162,48 @@ function W_star = trans_x_comp_w_star(x, y, lamda, dimension_start, dimension_en
     end
 end
 
-function w_n = onlineLMS(A,y,my, e_threshold)
-    % implement online learn from formula
-    % see http://de.wikipedia.org/wiki/LMS-Algorithmus
-    %e(n) = y(n) - x(n)'*w(n);
-    %w(n+1) = w(n) + my* e(n) * x(n);    
+% function w_n = onlineLMS(A,y,mu, e_threshold)
+%     % implement online learn from formula
+%     % see http://de.wikipedia.org/wiki/LMS-Algorithmus
+%     %e(n) = y(n) - x(n)'*w(n);
+%     %w(n+1) = w(n) + mu* e(n) * x(n);    
+%     [m,n] = size(A);
+%     %w_n = ones(m,1);
+%     w_n = zeros(m,1);
+%     cont = 1;
+%     while cont
+%         cont = 0;
+%         for n_index=1:n
+%             x_n = A(:,n_index);
+%             y_n = y(n_index);
+%             e_n = y_n - x_n'*w_n; % e_n ... error of equation n
+%             if e_n > e_threshold
+%                 cont = 1;
+%             end
+%             w_n = w_n + mu*e_n*x_n;
+%         end
+%     end
+% end
+
+function w_t = onlineLMS(A,t,lamda, E_threshold)
+    % implement online learn from formula    
+    %w(t+1) = w(t) + lamda* (t(i) - o(i)) * x(i);    
+    
     [m,n] = size(A);
-    w_n = ones(m,1);
-    cont = 1;
-    while cont
-        cont = 0;
-        for n_index=1:n
-            x_n = A(:,n_index);
-            y_n = y(n_index);
-            e_n = y_n - x_n'*w_n; % e_n ... error of equation n
-            if e_n > e_threshold
-                cont = 1;
-            end
-            w_n = w_n + my*e_n*x_n;
-        end
+    w_t = zeros(m,1);
+    E_tm1 = intmax;
+    E_t = 0;
+    while abs(E_t - E_tm1) > E_threshold
+        E_tm1 = E_t;
+        for i_index=1:n
+            x_i = A(:,i_index);
+            t_i = t(i_index);
+            o_i = w_t'*x_i;
+            % calculate cost
+            E_t = E_t + (t_i-o_i)^2;
+            % update w
+            w_t = w_t + lamda*(t_i-o_i)*x_i;
+        end            
     end
 end
 
